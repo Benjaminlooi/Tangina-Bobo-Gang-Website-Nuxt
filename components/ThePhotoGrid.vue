4e232d855d6ba9fa6a146869162d18b759f7d564
Benjamin Looi <benjaminlooidt@gmail.com>
2021-05-13 00:00:10 +0800

    Load additional images only when scrolled to bottom
    * .
    
    * .
    
    * Update ThePhotoGrid.vue
    
    * .
    
    * Update ThePhotoGrid.vue
    
    * Update ThePhotoGrid.vue

--- components/ThePhotoGrid.vue
+++ components/ThePhotoGrid.vue
@@ -1,32 +1,33 @@
 <template>
   <div>
     <p class>{{ gallery }}</p>
+    <!-- <button @click="loadImage()">Load image</button>
+    <button @click="initiateMansory()">Initiate mansory</button>
+    <button @click="refreshMasonry()">Refresh mansory</button> -->
     <div class="grid" id="gallery" ref="gallery">
       <div class="grid-item" v-for="(item, index) in items" :key="index">
-        <div>
-          <img
-            v-if="index > 8"
-            data-aos="fade-up"
-            :src="require('@/assets/img/' + gallery + '/' + item)"
-            class="image"
-          />
-          <img
-            v-else
-            data-aos="fade-up"
-            data-aos-offset="50"
-            :src="require('@/assets/img/' + gallery + '/' + item)"
-            class="image"
-          />
-        </div>
+        <img
+          v-if="index > 9"
+          data-aos="fade-up"
+          data-aos-anchor-placement="center-bottom"
+          :src="require('@/assets/img/' + gallery + '/' + item)"
+          class="image"
+        />
+        <img
+          v-else
+          data-aos="fade-up"
+          data-aos-offset="50"
+          :src="require('@/assets/img/' + gallery + '/' + item)"
+          class="image"
+        />
       </div>
     </div>
+
+    <div ref="loader"></div>
   </div>
 </template>
 
 <script>
-// import Masonry from 'masonry-layout'
-// import imagesLoaded from 'imagesloaded'
-// import AOS from 'aos'
 import { tgnbbtripMin, rayymondmpp, gangphotoshoot } from '@/misc/galleryLinks'
 
 export default {
@@ -37,6 +38,7 @@ export default {
   data: () => ({
     msnry: null,
     imgLoad: null,
+    imageLoadCount: 9,
   }),
   computed: {
     items() {
@@ -46,7 +48,7 @@ export default {
         case 'rayymondmpp':
           return [...rayymondmpp]
         case 'gangphotoshoot':
-          return [...gangphotoshoot]
+          return gangphotoshoot.slice(0, this.imageLoadCount)
         default:
           return null
       }
@@ -54,9 +56,40 @@ export default {
   },
   mounted() {
     if (process.browser) {
+      this.initiateMansory()
+
+      const loaderRef = this.$refs.loader
+
+      const scene = this.$scrollmagic
+        .scene({
+          triggerElement: loaderRef,
+          triggerHook: 'onEnter',
+          offset: -50,
+        })
+        .on('enter', (e) => {
+          this.loadImage()
+        })
+
+      this.$scrollmagic.addScene(scene)
+    }
+  },
+  methods: {
+    refreshMasonry() {
+      if (this.msnry) {
+        console.log('resetting Mansory...')
+        this.msnry.layout()
+      }
+    },
+    loadImage() {
+      this.imageLoadCount += 3
+      setTimeout(() => {
+        this.initiateMansory()
+      }, 0)
+    },
+    initiateMansory() {
+      const Masonry = require('masonry-layout')
       const AOS = require('aos')
       const imagesLoaded = require('imagesloaded')
-      const Masonry = require('masonry-layout')
 
       const galleryRef = this.$refs.gallery
 
@@ -65,30 +98,25 @@ export default {
       })
 
       this.imgLoad = imagesLoaded(galleryRef, () => {
+        console.log('done')
         this.refreshMasonry()
-        // galleryRef.style.display = 'block'
         AOS.refresh()
       })
 
       this.imgLoad.on('progress', (instance, image) => {
+        console.log('loaded')
         this.refreshMasonry()
+        AOS.refresh()
       })
-    }
-  },
-  methods: {
-    refreshMasonry() {
-      if (this.msnry) {
-        this.msnry.layout()
-      }
     },
   },
 }
 </script>
 
 <style lang="scss" scoped>
-.grid#gallery {
-  /* display: none; */
-}
+/* .grid#gallery {
+  display: none;
+} */
 
 .image {
   opacity: 0;
@@ -102,6 +130,10 @@ p {
   margin: 0 auto 100px;
   font-weight: 500;
   position: relative;
+}
+
+.grid {
+  min-height: 100vh;
 }
 
 .grid-item {
