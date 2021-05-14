6b94f7d6674cecaa6dbafa0c8f060a1ff28896d4
Benjamin Looi <benjaminlooidt@gmail.com>
2021-05-15 00:13:14 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: [
+    // https://stackoverflow.com/questions/66484296/some-tailwind-styles-not-working-in-production-with-next-js
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layout/**/*.{js,ts,jsx,tsx}',
+    './helpers/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
