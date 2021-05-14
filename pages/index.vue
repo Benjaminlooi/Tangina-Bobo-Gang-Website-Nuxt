0c0348ab137cd5ab693e5a44f8235580956d879c
Benjamin Looi <benjaminlooidt@gmail.com>
2021-05-14 23:58:04 +0800

    Add gallery showcase in index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,71 @@
 <template>
   <div id="index" ref="index">
-    <div class="hero">
-      <div class="hero-text-container">
+    <div ref="hero" class="hero">
+      <div ref="heroTextContainer" class="hero-text-container">
         <h1 ref="heroText" class="hero-text uppercase">Tangina Bobo Gang</h1>
       </div>
 
       <HeroSlider class="hero-slider" />
     </div>
 
-    <div class="section-container">
-      <div class="cell">
+    <div class="section-container sabah-trip">
+      <div class="cell left">
         <div ref="firstSectionBgContext" class="bg-context">
           <div class="bg"></div>
         </div>
       </div>
 
-      <div class="cell">
-        <!--  -->
+      <div class="cell right">
+        <nuxt-link exact to="/tanginabobo-trip" class="gallery-showcase">
+          <div class="visual-context">
+            <h2 class="visual-text">
+              <p>Sabah trip</p>
+              <button>View gallery</button>
+            </h2>
+            <div class="visual"></div></div
+        ></nuxt-link>
       </div>
     </div>
 
-    <div class="section-container">
-      <div class="cell"></div>
+    <div class="section-container photoshoot">
+      <div class="cell left">
+        <nuxt-link exact to="/photoshoot" class="gallery-showcase">
+          <div class="visual-context">
+            <h2 class="visual-text">
+              <p>Photoshoot</p>
+              <button>View gallery</button>
+            </h2>
+            <div class="visual"></div></div
+        ></nuxt-link>
+      </div>
 
-      <div class="cell">
+      <div class="cell right">
         <div ref="secondSectionBgContext" class="bg-context">
           <div class="bg"></div>
         </div>
       </div>
     </div>
 
-    <div style="height: 400px; background: #fff; position: relative">
-      really long footer
+    <div class="section-container mpp">
+      <div class="cell left">
+        <div ref="thirdSectionBgContext" class="bg-context">
+          <div class="bg"></div>
+        </div>
+      </div>
+
+      <div class="cell right">
+        <nuxt-link exact to="/rayymondmpp" class="gallery-showcase">
+          <div class="visual-context">
+            <h2 class="visual-text">
+              <p>Jerome MPP</p>
+              <button>View gallery</button>
+            </h2>
+            <div class="visual"></div></div
+        ></nuxt-link>
+      </div>
     </div>
+
+    <!-- 3 -->
   </div>
 </template>
 
@@ -48,11 +81,15 @@ export default {
     this.landingTl = this.$gsap.timeline()
 
     this.landingTl
-      .to('.hero-slider', {
-        duration: 1,
-        scale: 1,
-        ease: 'power2.out',
-      })
+      .to(
+        '.hero-slider',
+        {
+          duration: 1,
+          scale: 1,
+          ease: 'power2.out',
+        },
+        '+=0.3'
+      )
       .to(
         '.hero-slider',
         {
@@ -68,9 +105,19 @@ export default {
         duration: 1,
         ease: 'power3.out',
       })
+      .to(this.$refs.heroTextContainer, {
+        y: '300%',
+        scrollTrigger: {
+          trigger: this.$refs.hero,
+          start: 'top top',
+          end: 'bottom top',
+          scrub: 0.5,
+          markers: false,
+        },
+      })
 
     this.$ScrollTrigger.defaults({
-      markers: true,
+      markers: false,
     })
 
     this.indexTl = this.$gsap.timeline()
@@ -106,11 +153,23 @@ export default {
       markers: false,
       pinSpacing: false,
     })
+
+    this.$ScrollTrigger.create({
+      trigger: this.$refs.thirdSectionBgContext,
+      pin: true,
+      scrub: 0,
+      markers: false,
+      pinSpacing: false,
+    })
   },
 }
 </script>
 
 <style lang="scss" scoped>
+#index {
+  overflow-x: hidden;
+}
+
 .hero {
   display: block;
   position: relative;
@@ -172,6 +231,48 @@ export default {
     width: 100%;
   }
 
+  &.sabah-trip {
+    .bg {
+      background-image: url('~/assets/img/gallary-showcase/sabah-trip.jpg');
+    }
+
+    .visual {
+      background-image: url('~/assets/img/gallary-showcase/sabah-trip-2.jpg');
+    }
+  }
+
+  &.photoshoot {
+    right: 0;
+
+    .visual-text {
+      color: #fff !important;
+    }
+
+    .bg {
+      background-image: url('~/assets/img/gallary-showcase/photoshoot.jpg');
+    }
+
+    .visual {
+      background-image: url('~/assets/img/gallary-showcase/photoshoot-2.jpg');
+    }
+  }
+
+  &.mpp {
+    right: 0;
+
+    .visual-text {
+      color: #fff !important;
+    }
+
+    .bg {
+      background-image: url('~/assets/img/gallary-showcase/jerome-mpp.jpg');
+    }
+
+    .visual {
+      background-image: url('~/assets/img/gallary-showcase/jerome-mpp.jpg');
+    }
+  }
+
   .cell {
     width: 50%;
     height: 100%;
@@ -180,18 +281,10 @@ export default {
 
     &:first-child {
       left: 0;
-
-      .bg {
-        background-image: url('~/assets/img/hero/hero1.jpg');
-      }
     }
 
     &:nth-child(2) {
       right: 0;
-
-      .bg {
-        background-image: url('~/assets/img/hero/hero2.jpg');
-      }
     }
 
     .bg-context {
@@ -210,6 +303,60 @@ export default {
         display: block;
         height: 100%;
         width: 100%;
+      }
+    }
+
+    .gallery-showcase {
+      align-items: flex-end;
+      display: flex;
+      height: 100%;
+      padding: 110px;
+      position: relative;
+      width: 100%;
+
+      .visual-context {
+        width: 100%;
+        position: relative;
+
+        &::before {
+          display: block;
+          content: '';
+          width: 100%;
+          padding-top: 114.2405063291%;
+        }
+
+        .visual-text {
+          position: absolute;
+          top: 50%;
+          left: 0;
+          transform: translateY(-50%);
+          width: 100%;
+          color: #000;
+          z-index: 1;
+          text-align: center;
+
+          p {
+            font-weight: bold;
+            text-transform: uppercase;
+          }
+
+          button {
+            margin-top: 30px;
+            text-decoration: underline;
+          }
+        }
+
+        .visual {
+          top: 0;
+          left: 0;
+          position: absolute;
+          background-size: cover;
+          background-repeat: no-repeat;
+          background-position: 50%;
+          display: block;
+          height: 100%;
+          width: 100%;
+        }
       }
     }
   }
