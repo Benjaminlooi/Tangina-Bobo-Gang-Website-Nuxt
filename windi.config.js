13b36448e089dad679b369ae8f04bab0bce46b9c
Benjamin Looi <benjaminlooidt@gmail.com>
2022-05-28 13:45:15 +0800

    Migrate from tailwindcss to windicss #5

---
+++ windi.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ['Josefin Sans'],
+      // serif: ['Josefin Sans'],
+      // mono: ['Josefin Sans'],
+      // display: ['Josefin Sans'],
+      // body: ['Josefin Sans'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
